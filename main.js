9e9df9faf1495cc702e161a6aa85ab15a9894497
Bobby <36115061+bobbywestman@users.noreply.github.com>
2018-08-06 00:47:20 -0500

    darker background, added bow + arrows

--- main.js
+++ main.js
@@ -6,12 +6,14 @@ window.onload = function () {
 var fadeInTitle = function() {
     var title = document.getElementById("title");
     title.classList.add("fadeInTitle");
-    setTimeout(fadeInArrow, 650);
+    setTimeout(fadeInArrow, 850);
 };
 
 var fadeInArrow = function() {
-    var arrow = document.getElementById("arrow");
-    arrow.classList.add("fadeInArrow");
+    // var arrow = document.getElementById("arrow");
+    var bow = document.getElementById("bow");
+    // arrow.classList.add("fadeInArrow");
+    bow.classList.add("fadeInArrow");
 };
 
 window.onscroll = function(){
