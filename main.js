686f604f2c28c132364130f49e7fc4dc356d5786
Bobby <36115061+bobbywestman@users.noreply.github.com>
2018-08-10 00:34:12 -0500

    lots of changes for projects section

--- main.js
+++ main.js
@@ -1,7 +1,104 @@
-window.onload = function () {
-    setTimeout(fadeInTitle, 650);
-}
+/**********************************************************/
+/*data*/
+/**********************************************************/
+var projects = [
+    {
+        id: "musicVisualizer",
+        name: "3D Music Visualizer",
+        description: "A music visualization experiment in 3D",
+        link: "https://codepen.io/bobbywestman/pen/YjVPZz?editors=1111",
+        tools: [
+            {
+                tool:"Three.js",
+                link:"https://threejs.org/"
+            }
+        ]
+    },
+    {
+        id: "flappyKanye",
+        name: "Flappy Kanye",
+        description: "A Flappy Bird clone featuring Kanye West",
+        link: "https://play.google.com/store/apps/details?id=com.robertwestman.flappykanye",
+        tools: [
+            {
+                tool:"Stencyl",
+                link:"http://www.stencyl.com/"
+            }
+        ]
+    },
+    {
+        id: "aethera",
+        name: "Aethera",
+        description: "A gravity based game in an ambient environment",
+        link: "https://play.google.com/store/apps/details?id=com.robertwestman.aethera",
+        tools: [
+            {
+                tool:"Stencyl",
+                link:"http://www.stencyl.com/"
+            }
+        ]
+    }
+];
 
+/**********************************************************/
+/*init*/
+/**********************************************************/
+window.onload = function() {
+    setTimeout(fadeInTitle, 650);
+
+    setupProjectTiles();
+};
+
+/**********************************************************/
+/*projects*/
+/**********************************************************/
+
+var setupProjectTiles = function() {
+    for(var p in projects) {
+        var tile = document.getElementById(projects[p].id);
+
+        tile.onclick = (function(p) {
+            return function() { projectClicked(projects[p]); }
+        })(p);
+    }
+
+    var focus = document.getElementById("focusedProject");
+    focus.style.opacity = 1;
+    focus.style.display = "none";
+};
+
+var aProjectIsCurrentlySelected = false;
+
+var projectClicked = function(project) {
+    var grid = document.getElementById("projectsGrid");
+    var focus = document.getElementById("focusedProject");
+
+    if(aProjectIsCurrentlySelected) {
+        aProjectIsCurrentlySelected = false;
+
+        focus.removeChild(focus.firstChild);
+
+        focus.style.display = "none";
+        grid.style.display = "block";
+    }
+    else {
+        aProjectIsCurrentlySelected = true;
+        
+        var copy = document.getElementById(project.id).cloneNode(true);
+        //reattach click event on clone.. this is messy
+        copy.onclick = (function(project) {
+            return function() { projectClicked(projects[project]); }
+        })(project);
+        focus.insertBefore(copy, focus.firstChild);
+        
+        grid.style.display = "none";
+        focus.style.display = "block";
+    }
+};
+
+/**********************************************************/
+/*animations*/
+/**********************************************************/
 var fadeInTitle = function() {
     var title = document.getElementById("title");
     title.classList.add("fadeInTitle");
@@ -9,26 +106,6 @@ var fadeInTitle = function() {
 };
 
 var fadeInArrow = function() {
-    // var arrow = document.getElementById("arrow");
     var bow = document.getElementById("bow");
-    // arrow.classList.add("fadeInArrow");
     bow.classList.add("fadeInArrow");
 };
-
-window.onscroll = function(){
-    // showHideArrow()
-};
-
-function showHideArrow()
-{
-    var scrollBarPosition = window.pageYOffset | document.body.scrollTop;
-
-    if(scrollBarPosition > window.innerHeight * .4) {
-        document.getElementById('arrow').style.visibility = "hidden";
-    }
-    else {
-        document.getElementById('arrow').style.visibility = "visible";
-    }
-};
-
-
