80a6c35e7d67fef667d5c7bb06868f18ffa0d422
Bobby <36115061+bobbywestman@users.noreply.github.com>
2018-08-18 22:41:27 -0500

    finished focused project section

--- main.js
+++ main.js
@@ -4,13 +4,13 @@
 var projects = [
     {
         id: "musicVisualizer",
-        name: "3D Music Visualizer",
+        name: "Music Visualizer",
         description: "A music visualization experiment in 3D",
-        link: "https://codepen.io/bobbywestman/pen/YjVPZz?editors=1111",
+        link: "https://codepen.io/bobbywestman/pen/YjVPZz",
         tools: [
             {
-                name:"Three.js",
-                link:"https://threejs.org/"
+                name: "Three.js",
+                link: "https://threejs.org/"
             }
         ]
     },
@@ -21,8 +21,8 @@ var projects = [
         link: "https://play.google.com/store/apps/details?id=com.robertwestman.flappykanye",
         tools: [
             {
-                name:"Stencyl",
-                link:"http://www.stencyl.com/"
+                name: "Stencyl",
+                link: "http://www.stencyl.com/"
             }
         ]
     },
@@ -33,8 +33,8 @@ var projects = [
         link: "https://play.google.com/store/apps/details?id=com.robertwestman.aethera",
         tools: [
             {
-                name:"Stencyl",
-                link:"http://www.stencyl.com/"
+                name: "Stencyl",
+                link: "http://www.stencyl.com/"
             }
         ]
     }
@@ -66,16 +66,16 @@ var setupProjectTilesClickActions = function() {
 var aProjectIsCurrentlySelected = false;
 
 var projectClicked = function(project) {
-    scrollToProjects();
-
     var grid = document.getElementById("projectsGrid");
     var focus = document.getElementById("focusedProject");
     var focusedProjectGrid = document.getElementById("focusedProjectGrid");
+    var name = document.getElementById("focusedProjectNameLink");
+    var description = document.getElementById("focusedProjectDescription");
+    var tools = document.getElementById("focusedProjectTools");
 
     if(aProjectIsCurrentlySelected) {
         aProjectIsCurrentlySelected = false;
 
-        //remove clone
         focusedProjectGrid.removeChild(document.getElementById("clone"));
 
         focus.style.display = "none";
@@ -84,34 +84,62 @@ var projectClicked = function(project) {
     else {
         aProjectIsCurrentlySelected = true;
         
-        //clone the selected project tile
         var copy = document.getElementById(project.id).cloneNode(true);
         copy.setAttribute("id", "clone");
-
-        //reattach click event on clone since .cloneNode() removes them..
-        //this is messy
+        //reattach click event on clone since .cloneNode() removes them.. //this is messy
         copy.onclick = (function(project) {
             return function() { projectClicked(projects[project]); }
         })(project);
-
-        //add clone
         focusedProjectGrid.insertBefore(copy, document.getElementById("focusedProjectInsert"));
         
+        name.innerHTML = project.name;
+        name.setAttribute("href", project.link);
+        description.innerHTML = project.description;
+        tools.innerHTML = "Made with: ";
+        for (var t in project.tools) {
+            tools.innerHTML += "<a target=\"_blank\" rel=\"noopener noreferrer\" href=\""
+                + project.tools[t].link
+                + "\">" + project.tools[t].name
+                + "</a>";
+
+            if(t != project.tools.length - 1) {
+                tools.innerHTML += ", ";
+            }
+        }
+
         grid.style.display = "none";
         focus.style.display = "block";
     }
+
+    scrollToProjects();
 };
 
 var scrollToProjects = function() {
-    setTimeout(scrollToProjectsScroll, 100);
+    //need this timeout to let focusedProject div finish laying out
+    setTimeout(scrollToProjectsScroll, 150);
 };
 
+var currentScrollPosition;
+
 var scrollToProjectsScroll = function() {
-    document.getElementById('projects').scrollIntoView({
-        behavior: 'smooth',
-        block: 'center',
-        inline: 'center',
-    });
+    if(aProjectIsCurrentlySelected) {
+        currentScrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+        document.getElementById("projects").scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+        });
+    }
+    else {
+        //not sure if i like this on/off
+
+        // window.scroll({
+        //     top: currentScrollPosition, 
+        //     left: 0, 
+        //     behavior: "smooth"
+        // });
+    }
 };
     
 /**********************************************************/
@@ -119,7 +147,7 @@ var scrollToProjectsScroll = function() {
 /**********************************************************/
 var startFadeInElements = function () {
     setTimeout(fadeInTitle, 650);
-    setTimeout(fadeInArrow, 1500);
+    setTimeout(fadeInBow, 1500);
 };
 
 var fadeInTitle = function() {
@@ -127,7 +155,7 @@ var fadeInTitle = function() {
     title.classList.add("fadeInTitle");
 };
 
-var fadeInArrow = function() {
+var fadeInBow = function() {
     var bow = document.getElementById("bow");
     bow.classList.add("fadeInArrow");
 };
